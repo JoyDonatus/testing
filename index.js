33908fecdc7a1213006403395a315b3e7e594f2d
Joy Donatus <112759639+JoyDonatus@users.noreply.github.com>
2024-02-27 23:36:00 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', () => {
+    const params = (new URL(document.location)).searchParams;
+    const name = params.get('firstName');
+    const pref = params.get('preference');
+
+    document.getElementById('firstNamePara').innerHTML = name;
+    document.getElementById('pref').innerHTML = pref;
+})
