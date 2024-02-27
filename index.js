ef8439faad6df49d0d71856a2d94911c71f463c6
Joy Donatus <112759639+JoyDonatus@users.noreply.github.com>
2024-02-27 23:51:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-window.addEventListener('load', () => {
-    const params = (new URL(document.location)).searchParams;
-    const name = params.get('firstName');
-    const pref = params.get('preference');
+// window.addEventListener('load', () => {
+//     const params = (new URL(document.location)).searchParams;
+//     const name = params.get('firstName');
+//     const pref = params.get('preference');
 
-    document.getElementById('firstNamePara').innerHTML = name;
-    document.getElementById('pref').innerHTML = pref;
-})
+//     document.getElementById('firstNamePara').innerHTML = name;
+//     document.getElementById('pref').innerHTML = pref;
+// })
